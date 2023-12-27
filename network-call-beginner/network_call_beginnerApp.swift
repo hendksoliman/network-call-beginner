4547ccd7e775a0323c08b94ecc8f4e9f9a6e5c52
hendksoliman <hend.k.soliman@gmail.com>
2023-12-27 18:33:51 +0200

    Initial Commit

---
+++ network-call-beginner/network_call_beginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  network_call_beginnerApp.swift
+//  network-call-beginner
+//
+//  Created by Hend Khaled Soliman on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct network_call_beginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
